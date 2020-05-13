4adf21c5f2230598df5d823802d7f1c5950a957b
sunil300 <sahoosunil250@gmail.com>
2020-05-13 15:12:07 +0530

    DbContextAndMigration are created

---
+++ Vega/Vega/Controllers/MakesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Vega.Models;
+using Vega.Persistence;
+
+namespace Vega.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MakesController : Controller
+    {
+        private readonly VegaDbContext context;
+
+        public MakesController(VegaDbContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet("/api/Makes")]
+        public async Task<IEnumerable<Make>> GetMakes()
+        {
+            return await context.Makes.Include(m => m.Models).ToListAsync();
+        }
+    }
+}
